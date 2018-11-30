c5e731c0331afb5fdfda8d79ba7f9e0dd71849e3
alexreiling <alexanderreiling@googlemail.com>
2018-11-30 11:59:54 +0100

    main process renders electron as sidebar

--- client/main/src/index.js
+++ client/main/src/index.js
@@ -1,18 +1,33 @@
 
 // Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
+const electron = require('electron')
+const {app, BrowserWindow} = electron;
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
 
 function createWindow () {
+  const {width, height} = electron.screen.getPrimaryDisplay().workAreaSize
+  
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 1400, height: 600})
+  mainWindow = new BrowserWindow({
+    width: Math.max(width/4,400),
+    height: height,
+    x:width*3/4,
+    y:0,
+    resizable: false,
+    movable: false,
+    frame:false,
+    backgroundColor: '#222222'
+
+  })
 
   // and load the index.html of the app.
   mainWindow.loadURL('http://localhost:3000/')
-
+  mainWindow.once('ready-to-show', ()=>{
+    mainWindow.show()
+  })
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
 
