00a6bd1a8694bafc3e6017dc07f7149a4626b295
alexreiling <alexanderreiling@googlemail.com>
2018-12-29 18:16:25 +0100

    minor changes to image wrapper

--- client/renderer/src/components/common/Image.js
+++ client/renderer/src/components/common/Image.js
@@ -2,22 +2,24 @@ import React, { Component } from 'react';
 import styled from 'styled-components';
 
 const Container = styled.div`
-  width: 100%;
   height: 100%;
   box-sizing:border-box;
   text-align: center;
+  display:flex;
+  
 `
 
 const Img = styled.img`
-  max-height:100%;
-  max-width:100%;
+    width:100%;
+    height:100%;
+    object-fit: scale-down;
 `
 class Image extends Component {
   render() {
     const {imgName} = this.props;
     return (
       <Container {...this.props}>
-        <Img  src={require(`../../../assets/img/${imgName}`)} />
+        <Img src={require(`../../../assets/img/${imgName}`)} />
       </Container>
     );
   }
