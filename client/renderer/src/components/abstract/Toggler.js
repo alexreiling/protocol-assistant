eb7615066019d590a9ae06a3a3db5b6172b7f9e2
alexreiling <alexanderreiling@googlemail.com>
2019-01-10 13:10:51 +0100

    toggler follows nav.button theme

--- client/renderer/src/components/abstract/Toggler.js
+++ client/renderer/src/components/abstract/Toggler.js
@@ -2,10 +2,12 @@ import React from 'react';
 import styled from 'styled-components';
 const Div = styled.div`
   text-align: center;
-  color: ${p => p.theme.font.color.gray};
+  background-color: ${p => p.theme.nav.button.inactive.backgroundColor};
+  color: ${p => p.theme.nav.button.inactive.color};
   cursor: pointer;
   :hover {
-    background-color: ${p => p.theme.button.hover.backgroundColor};
+    background-color: ${p => p.theme.nav.button.hover.backgroundColor};
+    color: ${p => p.theme.nav.button.hover.color};
   }
 `
 export default (props) => {
