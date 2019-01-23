a1fc24606640100717940ae2b3c12cb98c8721a9
alexreiling <alexanderreiling@googlemail.com>
2019-01-23 15:23:12 +0100

    toggleSwitch component

--- client/renderer/src/components/SimTools/index.js
+++ client/renderer/src/components/SimTools/index.js
@@ -1,11 +1,37 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
+import H2 from '../common/H2';
+import H3 from '../common/H3';
+import { theme } from '../../config';
+import ToggleSwitch from '../common/ToggleSwitch';
 
+const Layout = styled.div`
+  color: ${theme.font.color.dark};
+`
+const Section = styled.div`
+  border-top: 1px solid ${theme.gridline.color};
+  background-color: ${theme.bg.bright};
+  padding: 8px;
+  :first-child {
+    background-color: ${theme.bg.contrast};
+  }
+`
 class SimTools extends Component {
   render() {
     return (
-      <div>
-        Hi, there!
-      </div>
+      <Layout>
+        <Section>
+        <H2>eLisA SimTools</H2>
+        <p>Mit diesem Panel können verschiedene Betriebszustände von eLisA getestet werden.</p>
+
+        </Section>
+        <Section>
+          <H3>Aktivität/Einverständnis Versicherungsnehmer</H3>
+          <p>Steuert die Aktivität von eLisA, die abhängig von der Einverständniserklärung des Versicherungsnehmers ist.</p>
+          <ToggleSwitch
+            onChange={(checked)=>console.log(checked)}/>
+        </Section>
+      </Layout>
     );
   }
 }
---
+++ client/renderer/src/components/common/ToggleSwitch.js
@@ -0,0 +1,71 @@
+// source: https://www.w3schools.com/howto/howto_css_switch.asp
+
+import React, { Component } from 'react';
+import styled, {css} from 'styled-components';
+
+const height = 24;
+const padding = Math.round(height / 8.5);
+const Switch = styled.div`
+  position: relative;
+  display: inline-block;
+  width: ${(height-padding)*2 + 'px'};
+  height: ${height + 'px'};
+`
+
+/* The slider */
+const Slider = styled.span`
+  position: absolute;
+  cursor: pointer;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: ${p => p.checked ? '#2196F3' : '#ccc'};
+  -webkit-transition: .4s;
+  transition: .4s;
+  border-radius: ${height + 'px'};
+  
+  :before {
+    position: absolute;
+    content: "";
+    height: ${height - 2 * padding + 'px'};
+    width: ${height - 2 * padding + 'px'};
+    left: ${padding + 'px'};
+    bottom: ${padding + 'px'};
+    background-color: white;
+    -webkit-transition: .4s;
+    transition: .4s;
+    border-radius: 50%;
+    ${p => p.checked && css`
+      -webkit-transform: ${`translateX(${height - 2 * padding + 'px'})`};
+      -ms-transform: ${`translateX(${height - 2 * padding + 'px'})`};
+      transform: ${`translateX(${height - 2 * padding + 'px'})`};
+    `}
+
+  }
+
+`
+class ToggleSwitch extends Component{
+  constructor(){
+    super()
+    this.handleChange = this.handleChange.bind(this)
+    this.state = {
+      checked: false
+    }
+  }
+  handleChange(){
+    const {onChange} = this.props
+    this.setState({checked: !this.state.checked},
+      () => onChange && onChange(this.state.checked)
+    )
+  }
+  render() {
+    return (
+      <Switch class="switch" onClick={this.handleChange}>
+        <Slider checked={this.state.checked} className="slider round"></Slider>
+      </Switch>
+    );
+  }
+}
+
+export default ToggleSwitch;
