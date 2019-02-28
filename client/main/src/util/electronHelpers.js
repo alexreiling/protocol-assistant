83260943135ab466ad1ad25f5e694b2d1fd8e84b
alexreiling <alexanderreiling@googlemail.com>
2019-02-28 15:46:46 +0100

    removed some debug messaging

--- client/main/src/util/electronHelpers.js
+++ client/main/src/util/electronHelpers.js
@@ -1,6 +1,5 @@
 module.exports = {
   sendToWebContents: (browserWindow, message, params) => {
-    console.log('Sending to webContents:', 'Window:', browserWindow, 'Message:', message, 'params:', params)
     browserWindow.webContents.send(message, params)
   }
 }
--- client/renderer/src/util/electronHelpers.js
+++ client/renderer/src/util/electronHelpers.js
@@ -2,6 +2,5 @@ var userAgent = navigator.userAgent.toLowerCase();
 export const isElectron = userAgent.indexOf(' electron/') > -1
 export const ipcRenderer = isElectron ? window.electron.ipcRenderer : null;
 export function sendToMain(message,params) {
-  console.log('Sending to main:', 'Message:', message, 'params:', params)
   if(isElectron) ipcRenderer.send(message,params)  
 }
